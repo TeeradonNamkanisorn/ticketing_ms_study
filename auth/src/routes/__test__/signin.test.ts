091d2880c80799f8ee7853df2ddda8b69fc72f77
TeeradonNamkanisorn <theme.2541@gmail.com>
2024-03-31 23:03:33 +0700

    fixed test

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -40,7 +40,7 @@ it("responds with a cookie when given a valid credential", async () => {
       email: "theme@mail.com",
       password: "123456",
     })
-    .expect(400);
+    .expect(200);
 
   expect(response.get("Set-Cookie")).toBeDefined();
 });
